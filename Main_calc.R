95381108915023a5aedf9ee705302909e19ea36e
Jihoon Min <grcosmos@gmail.com>
2015-12-23 09:13:15 +0100

    Small fix for flat allocation
    - Correct the denominator and other _flat variables.
    - Add PNG export lines

--- Main_calc.R
+++ Main_calc.R
@@ -68,36 +68,51 @@ for (i in 1:n_draw) {
   totE <- rbind(totE, colSums(energy_FR)*N_hh_FR/10)  # Total indirect energy by decile
   
   totE_per_hh_sc <- rbind(totE_per_hh_sc, colSums(energy_sc_FR)) # Total indirect energy/hh by decile
-  intensity_sc <- rbind(intensity_sc, colSums(energy_sc_FR)/colSums(fd_decile[,2:12])) # indirect energy intensity by decile [MJ/EUR]
+  # intensity_sc <- rbind(intensity_sc, colSums(energy_sc_FR)/colSums(fd_decile[,2:12])) # indirect energy intensity by decile [MJ/EUR]
   intensity_sc1 <- rbind(intensity_sc1, colSums(energy_sc_FR)/colSums(fd_with_soctr)) # indirect energy intensity by decile [MJ/EUR]
   totE_sc <- rbind(totE_sc, colSums(energy_sc_FR)*N_hh_FR/10)  # Total indirect energy by decile
   
   totE_per_hh_sc_flat <- rbind(totE_per_hh_sc_flat, colSums(energy_sc_FR_flat)) # Total indirect energy/hh by decile
-  intensity_sc_flat <- rbind(intensity_sc_flat, colSums(energy_sc_FR_flat)/colSums(fd_decile[,2:12])) # indirect energy intensity by decile [MJ/EUR]
-  # intensity_sc1 <- rbind(intensity_sc1, colSums(energy_sc_FR)/colSums(fd_with_soctr)) # indirect energy intensity by decile [MJ/EUR]
+  # intensity_sc_flat <- rbind(intensity_sc_flat, colSums(energy_sc_FR_flat)/colSums(fd_decile[,2:12])) # indirect energy intensity by decile [MJ/EUR]
+  intensity_sc_flat <- rbind(intensity_sc_flat, colSums(energy_sc_FR_flat)/colSums(fd_with_soctr_flat)) # indirect energy intensity by decile [MJ/EUR]
   totE_sc_flat <- rbind(totE_sc_flat, colSums(energy_sc_FR_flat)*N_hh_FR/10)  # Total indirect energy by decile
 }
 
-# f1 <- ggplot(data = totE_per_hh, aes(x = X, y = Y, group = group) )  # lesion becomes a classifying 
+figure_path = "H:/MyDocuments/IO work/France/"
+png(filename = paste(figure_path, "1.1 Energy consumption per hh by decile (without soc tr).png", sep=""), width = 781, height = 553, units = "px")
 boxplot(totE_per_hh[,2:11], ylab ="Total indirect energy per hh [MJ]")
+dev.off()
+
+png(filename = paste(figure_path, "1.2 Energy intensity by decile (without soc tr).png", sep=""), width = 781, height = 553, units = "px")
 boxplot(intensity[,2:11], ylab ="Indirect energy intensity [MJ/EUR]")
+dev.off()
 # boxplot(totE[,2:11]/10^6, ylab ="Total indirect energy [TJ]")
 
+png(filename = paste(figure_path, "2.1 Energy consumption per hh by decile (incl. soc tr by quintile).png", sep=""), width = 781, height = 553, units = "px")
 boxplot(totE_per_hh_sc[,2:11], ylab ="Total indirect energy per hh incl. SocTr[MJ]")
-boxplot(intensity_sc[,2:11], ylab ="Indirect energy intensity incl. SocTr [MJ/EUR]")
-boxplot(intensity_sc1[,2:11], ylab ="Indirect energy intensity incl. SocTr [MJ/EUR]")
+dev.off()
+# boxplot(intensity_sc[,2:11], ylab ="Indirect energy intensity incl. SocTr [MJ/EUR]")
 
+png(filename = paste(figure_path, "2.2 Energy intensity by decile (incl. soc tr by quintile).png", sep=""), width = 781, height = 553, units = "px")
+boxplot(intensity_sc1[,2:11], ylab ="Indirect energy intensity incl. SocTr [MJ/EUR]")
+dev.off()
+
+png(filename = paste(figure_path, "3.1 Energy consumption per hh by decile (incl. soc tr flat alloc).png", sep=""), width = 781, height = 553, units = "px")
 boxplot(totE_per_hh_sc_flat[,2:11], ylab ="Total indirect energy per hh incl. SocTr flat alloc [MJ]")
+dev.off()
+
+png(filename = paste(figure_path, "3.2 Energy intensity by decile (incl. soc tr flat alloc).png", sep=""), width = 781, height = 553, units = "px")
 boxplot(intensity_sc_flat[,2:11], ylab ="Indirect energy intensity incl. SocTr flat alloc [MJ/EUR]")
+dev.off()
 
 # Get intensities by COICOP consumption sector
 ind_intensity <- vector()
-null_demand_int <- matrix(0, 9600, 109)
+null_demand_int <- matrix(0, 9600, n_sector_coicop)
 
 for (i in 1:n_draw) {
   bridge_COICOP_EXIO <- get_bridge_COICOP_EXIO()
   
-  unit_exio <- t(bridge_COICOP_EXIO) %*% diag(n_sector_coicop)
+  unit_exio <- t(bridge_COICOP_EXIO) %*% diag(n_sector_coicop)  # Identity mtx representing 1 EUR spending in each sector
   fd_unit <- null_demand_int
   fd_unit[FR_idx,] <- get_basic_price(unit_exio, "FR")
   
@@ -105,4 +120,4 @@ for (i in 1:n_draw) {
   
   ind_intensity <- rbind(ind_intensity, colSums(energy_int_FR)) # Total indirect energy/hh by decile
 }
-boxplot(ind_intensity, ylab ="Energy intensity by consumption sector [MJ/EUR]")
+boxplot(ind_intensity, xlab ="COICOP sectors", ylab ="Energy intensity by consumption sector [MJ/EUR]")
